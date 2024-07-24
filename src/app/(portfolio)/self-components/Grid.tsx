bd20e34e2c3d82c5dfe2947bfed33f349a1fcf08
attaryu <mattarannaufal@gmail.com>
2024-07-24 22:16:01 +0700

    perf: avoiding re-rendering every animation played

--- src/app/(portfolio)/self-components/Grid.tsx
+++ src/app/(portfolio)/self-components/Grid.tsx
@@ -1,62 +1,48 @@
 'use client';
 
+import gsap from 'gsap';
+import { memo, useEffect, useRef } from 'react';
+
 import useEvent from '@/hooks/useEvent';
 import useMatchMedia from '@/hooks/useMatchMedia';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-import { memo, useEffect, useState } from 'react';
-
-gsap.registerPlugin(useGSAP);
 
 export default function Grid() {
   // subsribe to loading timeline when done
   const { subscribe } = useEvent('timeline@loading');
-  const [play, setPlay] = useState(false);
-  const [totalBox, setTotalBox] = useState(0);
-  const random = () => Math.floor(Math.random() * (totalBox * 0.6));
-  const [boxIndex, setBoxIndex] = useState(random());
+  const totalBoxRef = useRef(0);
 
   useEffect(() => {
     if (screen) {
-      setTotalBox(Math.round(screen.height * 1.5));
+      totalBoxRef.current = Math.round(screen.height * 1.5);
     }
   }, []);
 
-  useEffect(() => {
-    const unsubscribe = subscribe(() => setPlay(true));
+  useMatchMedia(({ isSmall }) => subscribe(() => playAnimation(isSmall)), {
+    dependencies: [],
+    revert: true,
+  });
 
-    return unsubscribe(() => setPlay(false));
-  }, []);
-
-  useMatchMedia(
-    ({ isSmall }) => {
-      if (totalBox && play) {
-        gsap.to('.grid-box', {
-          borderColor: 'white',
-          stagger: {
-            from: boxIndex,
-            grid: 'auto',
-            amount: isSmall ? 2.5 : 1.5,
-            yoyo: true,
-            repeat: 1,
-            repeatDelay: 1.5,
-          },
-          onComplete: () => {
-            setTimeout(() => setBoxIndex(random()), 1500);
-          },
-        });
-      }
-    },
-    {
-      dependencies: [boxIndex, play],
-      revert: true,
-    },
-  );
+  function playAnimation(isSmall: boolean) {
+    gsap.to('.grid-box', {
+      borderColor: 'white',
+      stagger: {
+        from: Math.floor(Math.random() * (totalBoxRef.current * 0.6)),
+        grid: 'auto',
+        amount: isSmall ? 2.5 : 1.5,
+        yoyo: true,
+        repeat: 1,
+        repeatDelay: 1.5,
+      },
+      onComplete: () => {
+        setTimeout(() => playAnimation(isSmall), 1500);
+      },
+    });
+  }
 
   return (
     <div className="after:contents-[''] sticky top-0 grid h-[120svh] max-h-highest w-full grid-cols-[repeat(auto-fit,_minmax(2rem,_1fr))] overflow-hidden after:pointer-events-none after:absolute after:bottom-0 after:left-0 after:block after:h-1/5 after:w-full after:bg-gradient-to-t after:from-white after:to-transparent">
-      {!!totalBox &&
-        Array(totalBox)
+      {!!totalBoxRef.current &&
+        Array(totalBoxRef.current)
           .fill(1)
           .map((box, i) => <Box key={box + i} />)}
     </div>
